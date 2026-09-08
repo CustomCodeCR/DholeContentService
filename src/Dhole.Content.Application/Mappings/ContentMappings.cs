6c6e74d626508062f2f530c2417563a514c1e68a
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-07 21:53:47 -0600

    feat(content): map selected categories for editor

--- src/Dhole.Content.Application/Mappings/ContentMappings.cs
+++ src/Dhole.Content.Application/Mappings/ContentMappings.cs
@@ -8,12 +8,102 @@ using Dhole.Content.Domain.Media.Entities;
 using Dhole.Content.Domain.Navigation.Entities;
 using Dhole.Content.Domain.Settings.Entities;
 using Dhole.Content.Domain.Taxonomies.Entities;
+
 namespace Dhole.Content.Application.Mappings;
+
 public static class ContentMappings
 {
-    public static ContentItemDto ToDto(ContentItem x)=>new(x.Id,x.SiteKey,x.Type.ToString(),x.Status.ToString(),x.Title,x.Slug,x.Excerpt,x.BlocksJson,x.RenderedHtml,x.FeaturedMediaId,x.AuthorUserId,x.Locale,x.SortOrder,x.IsFeatured,new SeoDto(x.SeoTitle,x.SeoDescription,x.SeoKeywords,x.CanonicalUrl,x.Robots,x.OpenGraphMediaId,x.StructuredDataJson),x.ScheduledAtUtc,x.PublishedAtUtc,x.CreatedAtUtc,x.UpdatedAtUtc);
-    public static TaxonomyTermDto ToDto(TaxonomyTerm x)=>new(x.Id,x.SiteKey,x.Kind,x.Name,x.Slug,x.Description,x.ParentId,x.SortOrder,x.CreatedAtUtc,x.UpdatedAtUtc);
-    public static MediaDto ToDto(MediaReference x)=>new(x.Id,x.StorageFileId,x.FileName,x.ContentType,x.AltText,x.Caption,x.MetadataJson,x.CreatedAtUtc,x.UpdatedAtUtc);
-    public static NavigationMenuDto ToDto(NavigationMenu x)=>new(x.Id,x.SiteKey,x.Name,x.Location,x.IsActive,x.Items.OrderBy(i=>i.SortOrder).Select(i=>new NavigationMenuItemDto(i.Id,i.ParentId,i.Label,i.Url,i.ContentId,i.Target,i.SortOrder,i.IsVisible)).ToArray(),x.CreatedAtUtc,x.UpdatedAtUtc);
-    public static SiteSettingDto ToDto(SiteSetting x)=>new(x.Id,x.SiteKey,x.Key,x.ValueJson,x.IsPublic,x.CreatedAtUtc,x.UpdatedAtUtc);
+    public static ContentItemDto ToDto(ContentItem item) =>
+        new(
+            item.Id,
+            item.SiteKey,
+            item.Type.ToString(),
+            item.Status.ToString(),
+            item.Title,
+            item.Slug,
+            item.Excerpt,
+            item.BlocksJson,
+            item.RenderedHtml,
+            item.FeaturedMediaId,
+            item.AuthorUserId,
+            item.Locale,
+            item.SortOrder,
+            item.IsFeatured,
+            item.Taxonomies.Select(x => x.TaxonomyTermId).ToArray(),
+            new SeoDto(
+                item.SeoTitle,
+                item.SeoDescription,
+                item.SeoKeywords,
+                item.CanonicalUrl,
+                item.Robots,
+                item.OpenGraphMediaId,
+                item.StructuredDataJson
+            ),
+            item.ScheduledAtUtc,
+            item.PublishedAtUtc,
+            item.CreatedAtUtc,
+            item.UpdatedAtUtc
+        );
+
+    public static TaxonomyTermDto ToDto(TaxonomyTerm item) =>
+        new(
+            item.Id,
+            item.SiteKey,
+            item.Kind,
+            item.Name,
+            item.Slug,
+            item.Description,
+            item.ParentId,
+            item.SortOrder,
+            item.CreatedAtUtc,
+            item.UpdatedAtUtc
+        );
+
+    public static MediaDto ToDto(MediaReference item) =>
+        new(
+            item.Id,
+            item.StorageFileId,
+            item.FileName,
+            item.ContentType,
+            item.AltText,
+            item.Caption,
+            item.MetadataJson,
+            item.CreatedAtUtc,
+            item.UpdatedAtUtc
+        );
+
+    public static NavigationMenuDto ToDto(NavigationMenu item) =>
+        new(
+            item.Id,
+            item.SiteKey,
+            item.Name,
+            item.Location,
+            item.IsActive,
+            item.Items
+                .OrderBy(x => x.SortOrder)
+                .Select(x => new NavigationMenuItemDto(
+                    x.Id,
+                    x.ParentId,
+                    x.Label,
+                    x.Url,
+                    x.ContentId,
+                    x.Target,
+                    x.SortOrder,
+                    x.IsVisible
+                ))
+                .ToArray(),
+            item.CreatedAtUtc,
+            item.UpdatedAtUtc
+        );
+
+    public static SiteSettingDto ToDto(SiteSetting item) =>
+        new(
+            item.Id,
+            item.SiteKey,
+            item.Key,
+            item.ValueJson,
+            item.IsPublic,
+            item.CreatedAtUtc,
+            item.UpdatedAtUtc
+        );
 }
