7d1caa3f478cd7d94cf59ff07b331b4c37d27db1
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-12 11:50:00 -0600

    Scope ContentMedia removal to parent content

--- src/Dhole.Content.Application/Features/Media/RemoveContentMedia/RemoveContentMediaCommand.cs
+++ src/Dhole.Content.Application/Features/Media/RemoveContentMedia/RemoveContentMediaCommand.cs
@@ -3,4 +3,4 @@ using CustomCodeFramework.Cqrs.Commands;
 
 namespace Dhole.Content.Application.Media.RemoveContentMedia;
 
-public sealed record RemoveContentMediaCommand(Guid Id, Guid? ActorUserId) : ICommand<Result>;
+public sealed record RemoveContentMediaCommand(Guid ContentId, Guid Id, Guid? ActorUserId) : ICommand<Result>;
