12fa0b5d67e0a682e62db4c75ecb3275e46b3e73
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-12 11:28:01 -0600

    feat(routes): configure content routes persistence

---
+++ src/Dhole.Content.Persistence/Configurations/Routes/ContentRouteConfiguration.cs
@@ -0,0 +1,41 @@
+using CustomCodeFramework.Postgres.EntityFramework.Configurations;
+using Dhole.Content.Domain.ContentItems.Entities;
+using Dhole.Content.Domain.Routes.Entities;
+using Dhole.Content.Domain.Sites.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dhole.Content.Persistence.Configurations.Routes;
+
+internal sealed class ContentRouteConfiguration : EntityTypeConfigurationBase<ContentRoute, Guid>
+{
+    public override void Configure(EntityTypeBuilder<ContentRoute> builder)
+    {
+        base.Configure(builder);
+        builder.ToTable("content_routes");
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        builder.Property(x => x.SiteKey).HasMaxLength(80).IsRequired();
+        builder.Property(x => x.Locale).HasMaxLength(20).IsRequired();
+        builder.Property(x => x.Path).HasMaxLength(1200).IsRequired();
+
+        builder.HasIndex(x => new { x.SiteKey, x.Locale, x.Path })
+            .IsUnique();
+
+        builder.HasIndex(x => new { x.ContentId, x.Locale })
+            .IsUnique()
+            .HasFilter("is_primary = true");
+
+        builder.HasIndex(x => new { x.SiteKey, x.ContentId, x.Locale });
+
+        builder.HasOne<ContentItem>()
+            .WithMany()
+            .HasForeignKey(x => x.ContentId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Site>()
+            .WithMany()
+            .HasForeignKey(x => x.SiteKey)
+            .HasPrincipalKey(x => x.SiteKey)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
